9d0bca06e63c0c384ec59a5b92ba3fc4bbc6820e
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-10-28 14:50:16 +0530

    Add files via upload

---
+++ Basic programme/odd & even number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\n enter the number=");
+scanf("%d",&num);
+if (num%2==0)
+
+printf("\n is an even number");
+
+else
+printf("\n is an odd number");
+
+return 0;
+}
