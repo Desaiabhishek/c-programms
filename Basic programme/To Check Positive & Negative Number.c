1664a59bff689fd45b5994ef393c091c4cb092e0
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-10-29 17:53:04 +0530

    Add files via upload

---
+++ Basic programme/To Check Positive & Negative Number.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\n enter the number=");
+scanf("%d",&num);
+if (num>=0)
+
+printf("\n %d is an positive  number",num);
+
+else
+printf("\n %d is an negative number",num);
+
+return 0;
+}
