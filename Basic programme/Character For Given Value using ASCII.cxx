0e7dea10bccbd5ddb24d84293d8ffc569a536c8d
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-10-30 19:27:05 +0530

    Add files via upload

---
+++ Basic programme/Character For Given Value using ASCII.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	int val= 0;
+	printf("\n Enter the value=");
+	scanf ("%d",&val);
+
+	printf("\n  character of ASCII value %d is =%c", val,val);
+
+	return 0;
+}
---
+++ Basic programme/Value For Given Character using ASCII.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+	char ch = '0';
+	printf("\n Enter the character=");
+	ch = getche();
+
+	printf("\n value of ASCII character %c is =%d", ch, ch);
+
+	return 0;
+}
