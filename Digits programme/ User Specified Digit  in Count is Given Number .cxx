546f7bcfa8e6a5b4ba762baa09d4fab940169427
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-10-30 12:49:40 +0530

    Add files via upload

---
+++ Digits programme/ User Specified Digit  in Count is Given Number .cxx
@@ -0,0 +1,25 @@
+#include<conio.h>
+#include<conio.h>
+int main()
+
+{
+
+int no=0,temp=0,dcount=0,dig=0;
+
+	printf("\n Enter Positive Number =");
+	scanf("%d",&no);
+	printf(" \n Enter to be counted in given number =");
+  scanf("%d"  ,&dig);
+    
+		temp=no;
+		while (temp>0)
+		{
+			
+			if((temp%10)==dig)
+			{
+				dcount++;
+			}
+				temp=temp/10;
+		}
+				printf("\n count of %d digit in %d number is =%d",dig,no,dcount);
+}
