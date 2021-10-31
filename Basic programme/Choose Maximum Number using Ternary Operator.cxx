3e2f80c0ae5191a3d9ebc924ca9252e6173c55fc
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-10-31 07:54:22 +0530

    Add files via upload

---
+++ Basic programme/Choose Maximum Number using Ternary Operator.cxx
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int no1=0,
+	     no2=0;
+	printf("\n enter the 2 numbers =");
+	scanf("%d%d",&no1,&no2);
+	if(no1== no2)
+{
+printf("\n both numbers are equal");
+}
+
+(no1>no2)? printf("\n %d is the maximun number",no1):printf("\n %d is the maximum number ",no2);
+	return 0;
+}
+		
+		
---
+++ Basic programme/Print India 5 times using for loop .cxx
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i=1;
+	for (i=1;i<=5;i++)
+	{
+		printf("\n INDIA");
+		
+	}
+	
+return 0;
+}
