f3831b9fd2bfc3bb4fc1a494e3e2181d20fb83f0
Desaiabhishek <93270535+Desaiabhishek@users.noreply.github.com>
2021-11-15 22:21:50 +0530

    Add files via upload

---
+++ Array/Array 2.cxx
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int values[5] =  {23,44,55,34,60};
+	
+	printf("\n values of 1st element = %d ",values[0]);
+	printf("\n values of 2nd element = %d ",values[1]);
+	printf("\n values of 3rd element = %d ",values[2]);
+	printf("\n values of 4th element = %d ",values[3]);
+	printf("\n values of 5th element = %d ",values[4]);
+	
+	return 0;
+}
