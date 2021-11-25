117b55f5d46196352cd83c182bbe3eb657d90c45
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2021-11-25 18:48:55 +0530

    Add files via upload

---
+++ Structure/01 Structure sizeof.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    struct stud
+    {
+        int Roll_No;
+        char Name[50];
+        char City[40];
+        long long int Mob_No;
+        int Phy;
+        int Maths;
+    };
+    int Num = 0;
+
+    struct stud Std1;
+
+    printf("\n Size Of Integer = %d",sizeof(int));
+    printf("\n Size Of Integer Variable = %d",sizeof(Num));
+    printf("\n Size Of Student Structure = %d",sizeof(struct stud));
+    printf("\n Size Of Student Structure Object = %d",sizeof(Std1));
+
+    getch();
+    return 0;
+}
