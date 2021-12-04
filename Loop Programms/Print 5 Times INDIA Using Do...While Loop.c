9a31efb40ecfdcddeb73eecd048f833c9f861c6f
Abhishek Desai <93270535+Desaiabhishek@users.noreply.github.com>
2021-12-04 19:35:32 +0530

    Add files via upload

---
+++ Loop Programms/Print 5 Times INDIA Using Do...While Loop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main ()
+{
+    int i = 0;
+
+    do
+    {
+        printf("\n INDIA");
+        i ++;
+    }while(i < 5);
+
+    getch ();
+    return 0;
+}
