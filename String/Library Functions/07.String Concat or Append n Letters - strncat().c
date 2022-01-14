847df5a46185c6f680cfb0ead9b8c5b01928c0a8
Desaiabhishek <desaiabhi2019@gmail.com>
2022-01-14 19:41:20 +0530

    Library Functions

---
+++ String/Library Functions/07.String Concat or Append n Letters - strncat().c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+    char FName[20] = "";
+    char SName[20] = "";
+
+    printf("\n Enter Your First Name = ");
+    gets(FName);
+
+    printf("\n Enter Your Surname =");
+    gets(SName);
+
+    getch();
+
+    strncat(FName,SName,3);
+
+    printf("\n First Name After String Concat = %s",FName);
+
+    printf("\n Surname After String Concat = %s",SName);
+
+    getch();
+    return 0;
+}
